266c6e26b44289ede702473a7700ff0a93e9d1f6
Fariba Massah <massah.2@gmail.com>
2017-01-27 13:45:48 -0800

    Finish tree search methods

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -36,4 +36,23 @@ class PolyTreeNode
     @value.dup
   end
 
+  def dfs(target_value)
+    return self if @value == target_value
+    @children.each do |child|
+      res = child.dfs(target_value) #display res debugger
+      return res if res
+    end
+    nil
+  end
+
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      first = queue.shift
+      return first if first.value == target_value
+      queue += first.children
+    end
+    nil
+  end
+
 end
